17779f6f9de9be9dfcb1b3b5ac738e6df3dce279
Arthur Toper <arthur@polykey.co.uk>
2014-03-12 15:46:54 +0000

    Fixed path issue in createlinks.sh.

--- createlinks.sh
+++ createlinks.sh
@@ -2,7 +2,9 @@
 
 shopt -s dotglob
 
-CONFIG_FILES=./home/*
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "Looking for config files in $DIR/home/..."
+CONFIG_FILES=$DIR/home/*
 for c in $CONFIG_FILES
 do
 	LINK_NAME=$(basename $c)
